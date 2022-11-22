9a30e6432c0b5b957b959c748133f42d60b7ebe5
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-22 07:52:07 +0530

    calculate daily employee wagee

--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -4,18 +4,24 @@
     {
         static void Main(string[] args)
         {
+            // Constants
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            // Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-            // Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present.");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
         }
     }
-}
