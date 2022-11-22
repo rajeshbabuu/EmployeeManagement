593453f2993159d70d9bac565041f46612e1b44a
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-22 07:49:47 +0530

    employee should be present or absent

---
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeeManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            // Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
