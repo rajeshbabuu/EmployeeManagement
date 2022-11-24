7f7abda3c8bb13f354ab3176e20ffbf04361fb33
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-24 16:39:07 +0530

    ability to compute Employee wage for multiple companies

--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -5,10 +5,10 @@
 
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
-        static void Main(string[] args)
+        //public const int EMP_RATE_PER_HOUR = 20;
+        //public const int NUM_OF_WORKING_DAYS = 2;
+        //public const int MAX_HRS_IN_MONTH = 10;
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
             // Variables
             int empHrs = 0;
@@ -16,7 +16,7 @@
             int totalWorkingDays = 0;
             // Computation
 
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -36,8 +36,14 @@
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Day# : " + totalWorkingDays + "Emp Hrs : " + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Toatal Employee Wage : " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Toatal Employee Wage for company : " + company + "is" + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage("DMART", 20, 2, 10);
+            computeEmpWage("Reliance", 10, 4, 20);
 
         }
             
